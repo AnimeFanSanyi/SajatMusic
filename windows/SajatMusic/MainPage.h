2d9e72f6e674cfe6ed3733af4d25043010cb3e1b
AnimeFanSanyi <sandor.forizs75@gmail.com>
2026-08-01 21:16:56 +0200

    Configure Windows app build setup and project files

---
+++ windows/SajatMusic/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::SajatMusic::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::SajatMusic::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
